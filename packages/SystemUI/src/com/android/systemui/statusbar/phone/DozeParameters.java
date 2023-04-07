Neoteric-OS/android_frameworks_base

Delay display DOZE on LS => AOD only if blanking is not needed
There is a brightness flickering when entering Always On Display (AOD)
from lock screen. The reason for this is that config_displayBlanksAfterDoze
is not read correctly when entering AOD from lock screen.

Checking canControlUnlockedScreenOff() to know if blanking is needed.

Test: set config_displayBlanksAfterDoze and let device go to AOD from lock screen.
Bug: 245919479
Change-Id: I90c68d25d468f22d2c1a8c2c0c2ef5723f0bc7a6

packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
@@ -348,7 +348,7 @@ public class DozeParameters implements
     }
 
     private boolean willAnimateFromLockScreenToAod() {
-        return getAlwaysOn() && mKeyguardShowing;
+        return canControlUnlockedScreenOff() && mKeyguardShowing;
     }
 
     private boolean getBoolean(String propName, int resId) {

Twinchin12/frameworks_base-1

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
@@ -352,7 +352,7 @@ public class DozeParameters implements
     }
 
     private boolean willAnimateFromLockScreenToAod() {
-        return getAlwaysOn() && mKeyguardVisible;
+        return canControlUnlockedScreenOff() && mKeyguardVisible;
     }
 
     private boolean getBoolean(String propName, int resId) {