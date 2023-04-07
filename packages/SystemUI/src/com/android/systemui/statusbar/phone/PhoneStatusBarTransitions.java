Project-Xtended/frameworks_base

[SQUASHED] Status bar logo [1/2]
This is Merge commit of

Validus:-
1) https://github.com/ValidusOs/frameworks_base/commit/738711fb1ad59d6136538fccdc7b257f9a33ec68#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
2) https://github.com/ValidusOs/frameworks_base/commit/16f05ef9f810faef2e515bfd6ad69fa7a2e062b0#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
3) https://github.com/ValidusOs/frameworks_base/commit/2e78a613ee3d4ac2c01276d5f7ffed0fc2f3fe86#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
4) https://github.com/ValidusOs/frameworks_base/commit/576d1610b2f1056034ea9a5058fb73225d31dc71#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
5) https://github.com/ValidusOs/frameworks_base/commit/4cae0470cfbe21f126f4e54f13051edb5feec1d9#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
6) https://github.com/ValidusOs/frameworks_base/commit/4b3ad92c4a6f86e51d45c4b57e4faa84b3f560d1#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
7) https://github.com/ValidusOs/frameworks_base/commit/3b2ea41b645d2ffb72eef69e621435af8a84624b#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
8) https://github.com/ResurrectionRemix/android_frameworks_base/commit/f939656166ad1ebb4ff7ce5af00393cb9efc93cb#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b

CrDroid Android:-
Thanks @neobuddy89 For Logos

1) https://github.com/crdroidandroid/android_frameworks_base/commit/a09738a65fdb93bb8c428124dc6a2a621fab2db8

Change-Id: I86eb407557197229481940e565ab711eb56312b0

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mBattery, mClock, mNetworkTraffic, mCenterClock, mRightClock, mWeatherTextView, mWeatherImageView;
+    private View mLeftSide, mStatusIcons, mBattery, mClock, mNetworkTraffic, mLogoIcon, mLogoIconRight, mCenterClock, mRightClock, mWeatherTextView, mWeatherImageView;
 
     private Animator mCurrentAnimation;
 
@@ -53,6 +53,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
         mWeatherTextView = mView.findViewById(R.id.weather_temp);
         mWeatherImageView = mView.findViewById(R.id.weather_image);
+	mLogoIcon = mView.findViewById(R.id.status_bar_logo);
+	mLogoIconRight = mView.findViewById(R.id.status_bar_logo_right);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -98,7 +100,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mClock, newAlphaBC),
                     animateTransitionTo(mCenterClock, newAlphaBC),
                     animateTransitionTo(mRightClock, newAlphaBC),
-                    animateTransitionTo(mNetworkTraffic, newAlpha)
+                    animateTransitionTo(mNetworkTraffic, newAlpha),
+		    animateTransitionTo(mLogoIcon, newAlpha),
+		    animateTransitionTo(mLogoIconRight, newAlpha)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -113,6 +117,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mCenterClock.setAlpha(newAlphaBC);
             mRightClock.setAlpha(newAlphaBC);
             mNetworkTraffic.setAlpha(newAlpha);
+	    mLogoIcon.setAlpha(newAlpha);
+	    mLogoIconRight.setAlpha(newAlpha);
             mWeatherTextView.setAlpha(newAlphaBC);
             mWeatherImageView.setAlpha(newAlphaBC);
         }

Twinchin12/frameworks_base-1

Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -31,7 +31,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
 
     private final float mIconAlphaWhenOpaque;
 
-    private View mStartSide, mStatusIcons, mBattery, mClock, mCenterClock, mRightClock;
+    private View mStartSide, mStatusIcons, mBattery, mClock, mLogoIcon, mLogoIconRight, mCenterClock, mRightClock;
     private View mBatteryBars[] = new View[2];
 
     private Animator mCurrentAnimation;
@@ -51,6 +51,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mClock = statusBarView.findViewById(R.id.clock);
         mCenterClock = statusBarView.findViewById(R.id.center_clock);
         mRightClock = statusBarView.findViewById(R.id.right_clock);
+	mLogoIcon = statusBarView.findViewById(R.id.status_bar_logo);
+	mLogoIconRight = statusBarView.findViewById(R.id.status_bar_logo_right);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -98,7 +100,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mBatteryBars[1], newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
                     animateTransitionTo(mCenterClock, newAlphaBC),
-                    animateTransitionTo(mRightClock, newAlphaBC)
+                    animateTransitionTo(mRightClock, newAlphaBC),
+		    animateTransitionTo(mLogoIcon, newAlpha),
+		    animateTransitionTo(mLogoIconRight, newAlpha)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -114,6 +118,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mClock.setAlpha(newAlphaBC);
             mCenterClock.setAlpha(newAlphaBC);
             mRightClock.setAlpha(newAlphaBC);
+	    mLogoIcon.setAlpha(newAlpha);
+	    mLogoIconRight.setAlpha(newAlpha);
         }
     }
 }